ddb393c021f1b3b30564d48ea9d5b70c2ba62a08
tottaway <tottaway123@gmail.com>
2021-04-19 12:16:33 -0400

    bad p controller

--- src/controller/pid_controller.cpp
+++ src/controller/pid_controller.cpp
@@ -4,6 +4,7 @@
 
 #include "yaml.h"
 #include <iostream>
+#include <algorithm>
 
 namespace control_final {
 PIDController::PIDController(const std::string &filename)
@@ -16,6 +17,15 @@ PIDController::PIDController(const std::string &filename)
 void PIDController::react(std::vector<char> &pixs, Reference &u,
                           const Sensor &sensor) {
   State predicted_state = predict_state(pixs, sensor);
+  /* if (predicted_state.ball_pose.x == 0) { */
+  /*   predicted_state.ball_pose.x = m_last_x; */
+  /*   predicted_state.ball_pose.y = m_last_y; */
+  /* } */
+
+  predicted_state.ball_pose.x = std::max(-0.4, predicted_state.ball_pose.x);
+  predicted_state.ball_pose.x = std::min(0.4, predicted_state.ball_pose.x);
+  predicted_state.ball_pose.y = std::max(-0.4, predicted_state.ball_pose.y);
+  predicted_state.ball_pose.y = std::min(0.4, predicted_state.ball_pose.y);
   std::cout << "[" << predicted_state.ball_pose.x << ", "
             << predicted_state.ball_pose.y << "]" << std::endl;
 
@@ -27,15 +37,15 @@ void PIDController::react(std::vector<char> &pixs, Reference &u,
   u.table_pose.ydot = 0;
   u.table_pose.zdot = 0;
 
-  const double goal_theta_x = 0.01 * predicted_state.ball_pose.y;
-  const double goal_theta_y = 0.01 * predicted_state.ball_pose.x;
+  const double goal_theta_x = -0.1 * predicted_state.ball_pose.y;
+  const double goal_theta_y = -0.1 * predicted_state.ball_pose.x;
 
   // TODO: get dt
-  u.table_pose.theta_dot_x = (goal_theta_x - m_last_x) * 100;
-  u.table_pose.theta_dot_y = (goal_theta_x - m_last_y) * 100;
+  u.table_pose.theta_dot_x = (goal_theta_x - m_last_x) * 5;
+  u.table_pose.theta_dot_y = (goal_theta_y - m_last_y) * 5;
 
-  m_last_x = goal_theta_x + ((goal_theta_x - m_last_x) / 10);
-  m_last_y = goal_theta_y + ((goal_theta_y - m_last_y) / 10);
+  m_last_x = m_last_x + ((goal_theta_x - m_last_x) / 200);
+  m_last_y = m_last_y + ((goal_theta_y - m_last_y) / 200);
 }
 
 } // namespace control_final
